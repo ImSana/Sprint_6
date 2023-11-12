b1cfbe02af832c74c164d9daa8938d2b3273cbd5
ImSana <aasursyakov@gmail.com>
2023-11-12 15:42:01 +0300

    first commit

---
+++ data/persons.py
@@ -0,0 +1,5 @@
+class Persons:
+    PERSON_1 = {'name': 'Василий', 'lastname': 'Чапаев', 'address': 'Белогвардейская ул. 25',
+                'station': 'Бауманская', 'number': '70000000000'}
+    PERSON_2 = {'name': 'Анна', 'lastname': 'Попова', 'address': 'Леонова ул. 12',
+                'station': 'Саларьево', 'number': '79999999999'}
---
+++ locators/base_page_locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+    YA_LOGO = By.XPATH, "//*[@alt='Yandex']"
+    SCOOTER_LOGO = By.XPATH, "//*[@alt='Scooter']"
